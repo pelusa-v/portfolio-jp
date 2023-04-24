20c5608e0b5ec33759eb05b38a7283753db7fda1
pelusa-v <razor0164@gmail.com>
2023-04-23 22:25:53 -0500

    Uers functionalities done

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/pelusa-v/portfolio-jp/internal/infrastructure/api"
+)
+
+func main() {
+	engine := gin.Default()
+	api.RegisterUsersRouter(engine)
+	engine.Run(":8000")
+}
